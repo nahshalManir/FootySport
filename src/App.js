d9603fb65bc10957ec12c8962b681f14f2eeb649
nahsh <nahshal10058@gmail.com>
2023-03-12 12:58:47 +0530

    Initial commit

--- src/App.js
+++ src/App.js
@@ -3,11 +3,9 @@ import {
   BrowserRouter as Router,
   Routes,
   Route,
-  Link
 } from "react-router-dom";
 import Navbar from './components/Navbar';
 import Home from './components/Home';
-import News from './components/News';
 import MatchData from './components/MatchData';
 import League from './components/League';
 import Footer from './components/Footer';
@@ -20,13 +18,6 @@ function App() {
       <Routes>
         <Route exact path='/' element={<Home/>}/>
         <Route exact path="/:matchId" element={<MatchData />} />
-        {/* <Route exact path="/uefa-champions-league" element={<League/>} />
-        <Route exact path="/uefa-europa-league" element={<League/>} />
-        <Route exact path="/premier-league" element={<League/>} />
-        <Route exact path="/laliga" element={<League/>} />
-        <Route exact path="/bundesliga" element={<League/>} />
-        <Route exact path="/ligue-1" element={<League/>} />
-        <Route exact path="/serie-a" element={<League/>} /> */}
         <Route exact path="/:country/:league" element={<League/>} />
         <Route exact path="/:league" element={<League/>} />
       </Routes>
